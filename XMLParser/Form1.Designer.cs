a7f603342e408729143243ec8feebc453d72e905
Kevin Florio <kflorio98@gmail.com>
2020-03-12 23:27:04 -0700

    init

---
+++ XMLParser/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace XMLParser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Raw = new System.Windows.Forms.TextBox();
+            this.Parsebtn = new System.Windows.Forms.Button();
+            this.Parsed = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Raw
+            // 
+            this.Raw.Location = new System.Drawing.Point(12, 112);
+            this.Raw.Multiline = true;
+            this.Raw.Name = "Raw";
+            this.Raw.Size = new System.Drawing.Size(645, 421);
+            this.Raw.TabIndex = 0;
+            // 
+            // Parsebtn
+            // 
+            this.Parsebtn.Location = new System.Drawing.Point(591, 70);
+            this.Parsebtn.Name = "Parsebtn";
+            this.Parsebtn.Size = new System.Drawing.Size(138, 36);
+            this.Parsebtn.TabIndex = 1;
+            this.Parsebtn.Text = "Parse";
+            this.Parsebtn.UseVisualStyleBackColor = true;
+            this.Parsebtn.Click += new System.EventHandler(this.Parsebtn_Click);
+            // 
+            // Parsed
+            // 
+            this.Parsed.Location = new System.Drawing.Point(663, 112);
+            this.Parsed.Multiline = true;
+            this.Parsed.Name = "Parsed";
+            this.Parsed.Size = new System.Drawing.Size(645, 421);
+            this.Parsed.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1321, 551);
+            this.Controls.Add(this.Parsed);
+            this.Controls.Add(this.Parsebtn);
+            this.Controls.Add(this.Raw);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Raw;
+        private System.Windows.Forms.Button Parsebtn;
+        private System.Windows.Forms.TextBox Parsed;
+    }
+}
+
---
+++ XMLParser/Form1.cs
@@ -0,0 +1,286 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace XMLParser
+{
+    public class DCP
+    {
+        public string tagNumber;
+        public string result;
+        public DCP(string tag, string rslt)
+        {
+            tagNumber = tag;
+            result = rslt;
+        }
+    }
+
+    public partial class Form1 : Form
+    {
+        public List<DCP> dcps;
+        public string xml;
+        private List<string> errorCode;
+        private List<string> errorMessage;
+        public Form1()
+        {
+            InitializeComponent();
+            dcps = new List<DCP>();
+            errorCode = new List<string>();
+            errorMessage = new List<string>();
+        }
+
+        private void Parsebtn_Click(object sender, EventArgs e)
+        {
+            string rawData = Raw.Text;
+            if (rawData.Trim() == "")
+            {
+                return;
+            }
+            try
+            {
+                XDocument doc = XDocument.Parse(rawData);
+                XNamespace ns = "http://mes.health.ge.com";
+                XNamespace dcpns = "http://saveDCP.mes.health.ge.com";
+                IEnumerable<XElement> responses = doc.Descendants(dcpns + "OutputDCP");
+
+                foreach (XElement response in responses)
+                {
+                    string str1 = response.Descendants(dcpns + "dcpResult").Single().Value;
+                    string str2 = response.Descendants(dcpns + "dctTag").Single().Value;
+                    dcps.Add(new DCP(str2, str1));
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Parse failed: " + ex.Message.ToString());
+            }
+            /*
+             <dCPResults xmlns="http://saveDCP.mes.health.ge.com">
+                    <OutputDCP>
+                        <dcpResult>ACCEPT</dcpResult>
+                        <dctTag>TAG003</dctTag>
+                    </OutputDCP>
+                    <OutputDCP>
+                        <dcpResult>ACCEPT</dcpResult>
+                        <dctTag>TAG002</dctTag>
+                    </OutputDCP>
+                    <OutputDCP>
+                        <dcpResult>ACCEPT</dcpResult>
+                        <dctTag>TAG001</dctTag>
+                    </OutputDCP>
+                </dCPResults>
+             */
+        }
+
+
+        public void fillxml(out string XML)
+        {
+            XML = @" < soap:Envelope xmlns:soap=""http://schemas.xmlsoap.org/soap/envelope/"" xmlns:xsd=""http://www.w3.org/2001/XMLSchema"" xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"" >
+    < soap:Body >
+         < ns1:saveDCPResponse xmlns:ns1 = ""http://mes.health.ge.com"" >
+              < ns1:out>
+                   < consumedPartsResults xmlns = ""http://saveDCP.mes.health.ge.com"" xsi: nil = ""true"" />
+                       < dCPResults xmlns = ""http://saveDCP.mes.health.ge.com"" >
+                            < OutputDCP >
+                                < dcpResult > ACCEPT </ dcpResult >
+                                < dctTag > TAG003 </ dctTag >
+                            </ OutputDCP >
+                            < OutputDCP >
+                                < dcpResult > ACCEPT </ dcpResult >
+                                < dctTag > TAG002 </ dctTag >
+                            </ OutputDCP >
+                            < OutputDCP >
+                                < dcpResult > ACCEPT </ dcpResult >
+                                < dctTag > TAG001 </ dctTag >
+                            </ OutputDCP >
+                        </ dCPResults >
+                        < errorCode xmlns = ""http://saveDCP.mes.health.ge.com"" > 500 </ errorCode >
+                         < errorMessage xmlns = ""http://saveDCP.mes.health.ge.com"" > DCP saved without complete </ errorMessage >
+                          </ ns1:out>
+                       </ ns1:saveDCPResponse >
+                    </ soap:Body >
+                 </ soap:Envelope > ";
+        }
+    }
+}
+/* Random examples
+ 
+string xml = @"<messages> 
+                  <message subclass=""a"" context=""d"" key=""g""/> 
+                  <message subclass=""b"" context=""e"" key=""h""/> 
+                  <message subclass=""c"" context=""f"" key=""i""/> 
+               </messages>";
+
+var messagesElement = XElement.Parse(xml);
+var messagesList = (from message in messagesElement.Elements("message")
+                   select new 
+                    {
+                        Subclass = message.Attribute("subclass").Value,
+                        Context = message.Attribute("context").Value,
+                        Key = message.Attribute("key").Value
+                    }).ToList();     
+     
+     <soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+    <soap:Body>
+        <ns1:saveDCPResponse xmlns:ns1="http://mes.health.ge.com">
+            <ns1:out>
+                <consumedPartsResults xmlns="http://saveDCP.mes.health.ge.com" xsi:nil="true"/>
+                <dCPResults xmlns="http://saveDCP.mes.health.ge.com">
+                    <OutputDCP>
+                        <dcpResult>INVALID DCP IMPORT TAG</dcpResult>
+                        <dctTag>TAG009</dctTag>
+                    </OutputDCP>
+                </dCPResults>
+                <errorCode xmlns="http://saveDCP.mes.health.ge.com">-504</errorCode>
+                <errorMessage xmlns="http://saveDCP.mes.health.ge.com">Cannot save one or more DCP lines because of invalid DCP Import Tag</errorMessage>
+            </ns1:out>
+        </ns1:saveDCPResponse>
+    </soap:Body>
+</soap:Envelope>
+
+    <soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+    <soap:Body>
+        <ns1:saveDCPResponse xmlns:ns1="http://mes.health.ge.com">
+            <ns1:out>
+                <consumedPartsResults xmlns="http://saveDCP.mes.health.ge.com" xsi:nil="true"/>
+                <dCPResults xmlns="http://saveDCP.mes.health.ge.com">
+                    <OutputDCP>
+                        <dcpResult>ACCEPT</dcpResult>
+                        <dctTag>TAG002</dctTag>
+                    </OutputDCP>
+                </dCPResults>
+                <errorCode xmlns="http://saveDCP.mes.health.ge.com">500</errorCode>
+                <errorMessage xmlns="http://saveDCP.mes.health.ge.com">DCP saved without complete</errorMessage>
+            </ns1:out>
+        </ns1:saveDCPResponse>
+    </soap:Body>
+</soap:Envelope>
+*/
+
+/************************************** MESSAGE & RESPONSE **************************************
+    
+    * 
+    * MESSAGE- 
+    * 
+<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:mes="http://mes.health.ge.com" xmlns:sav="http://saveDCP.mes.health.ge.com">
+   <soapenv:Header/>
+   <soapenv:Body>
+      <mes:saveDCP>
+         <mes:in0>
+            <!--Optional:-->
+            <sav:completeOperation>0</sav:completeOperation>
+            <!--Optional:-->
+            <sav:consumedParts>
+            </sav:consumedParts>
+            <!--Optional:-->
+            <sav:dcps>
+               <!--Zero or more repetitions:-->
+               <sav:DCP>
+                  <!--Optional:-->
+                  <sav:dcpImportTag>TAG003</sav:dcpImportTag>
+                  <!--Optional:-->
+                  <sav:dcpValue>3</sav:dcpValue>
+               </sav:DCP>
+               <!--Zero or more repetitions:-->
+               <sav:DCP>
+                  <!--Optional:-->
+                  <sav:dcpImportTag>TAG002</sav:dcpImportTag>
+                  <!--Optional:-->
+                  <sav:dcpValue>2</sav:dcpValue>
+               </sav:DCP>
+               <!--Zero or more repetitions:-->
+               <sav:DCP>
+                  <!--Optional:-->
+                  <sav:dcpImportTag>TAG001</sav:dcpImportTag>
+                  <!--Optional:-->
+                  <sav:dcpValue>1</sav:dcpValue>
+               </sav:DCP>
+            </sav:dcps>
+            <!--Optional:-->
+            <sav:operationName>CTM_Gan-SubA</sav:operationName>
+            <!--Optional:-->
+            <sav:partNumber>5454001-170-SUBSA</sav:partNumber>
+            <!--Optional:-->
+            <sav:partRevision>N/A</sav:partRevision>
+            <!--Optional:-->
+            <sav:password>D1NPnWPE5WwnE9vatgdNzUFICLN1WA3GM6UCLN1WA3GM6U</sav:password>
+            <!--Optional:-->
+            <sav:reasonCode>PASS</sav:reasonCode>
+            <!--Optional:-->
+            <sav:routeName>CTM_Gan_SubA</sav:routeName>
+            <!--Optional:-->
+            <sav:serialNumber>WebTestAME001</sav:serialNumber>
+            <!--Optional:-->
+            <sav:userID>555333333</sav:userID>
+         </mes:in0>
+      </mes:saveDCP>
+   </soapenv:Body>
+</soapenv:Envelope>
+
+    * RESPONSE - 
+    * 
+<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+    <soap:Body>
+        <ns1:saveDCPResponse xmlns:ns1="http://mes.health.ge.com">
+            <ns1:out>
+                <consumedPartsResults xmlns="http://saveDCP.mes.health.ge.com" xsi:nil="true"/>
+                <dCPResults xmlns="http://saveDCP.mes.health.ge.com">
+                    <OutputDCP>
+                        <dcpResult>ACCEPT</dcpResult>
+                        <dctTag>TAG003</dctTag>
+                    </OutputDCP>
+                    <OutputDCP>
+                        <dcpResult>ACCEPT</dcpResult>
+                        <dctTag>TAG002</dctTag>
+                    </OutputDCP>
+                    <OutputDCP>
+                        <dcpResult>ACCEPT</dcpResult>
+                        <dctTag>TAG001</dctTag>
+                    </OutputDCP>
+                </dCPResults>
+                <errorCode xmlns="http://saveDCP.mes.health.ge.com">500</errorCode>
+                <errorMessage xmlns="http://saveDCP.mes.health.ge.com">DCP saved without complete</errorMessage>
+            </ns1:out>
+        </ns1:saveDCPResponse>
+    </soap:Body>
+</soap:Envelope>
+
+
+
+
+
+    ************************** WORKS FOR ERROR CODE ************************
+    *                 XDocument doc = XDocument.Parse(rawData);
+                XNamespace ns = "http://mes.health.ge.com";
+                XNamespace dcpns = "http://saveDCP.mes.health.ge.com";
+                IEnumerable<XElement> responses = doc.Descendants(dcpns + "errorCode");
+                
+                foreach (XElement response in responses)
+                {
+                    if (Int32.TryParse(response.Value, out int i))
+                    { 
+                        if (i == 500 || i == 0)
+                        {
+                            // Success
+                            // return
+                            errorCode.Add(response.Value);
+                        }
+                        else
+                        {
+                            // Error status returned
+                        }
+                    }
+                    // Error occured
+
+                }
+
+
+*/
